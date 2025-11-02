bc365a82c61c21e403359d9ad039e92008bfeafe
Andrii Nykolenko <nikolandre14@gmail.com>
2025-11-02 17:59:02 +0200

    Add user can sign in test

---
+++ tests/e2e/login.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from "@playwright/test";
+
+test.describe.only("Login", () => {
+  test("User can sign in", async ({ page }) => {
+    await page.goto("https://conduit.mate.academy/user/login");
+
+    const emailField = page.getByPlaceholder(/Email/);
+    const passwordField = page.getByPlaceholder("Password");
+    const submitBtn = page.getByRole("button", { name: "Sign in" });
+
+    await emailField.fill("nikolandre14@gmail.com");
+    await passwordField.fill("12345678");
+    await submitBtn.click();
+
+    await expect(page).toHaveURL("https://conduit.mate.academy");
+
+    const profile = page.locator(".nav-link", { has: page.locator(".user-pic") });
+    await expect(profile).toBeVisible();
+    await expect(profile).toContainText("nap1408")
+  });
+});
